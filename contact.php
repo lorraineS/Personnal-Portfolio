c22a03388ca33ba4626d179be83c4e4cd88cd998
lorraineS <lorraine.univ@gmail.com>
2016-02-02 17:34:38 +0100

    Update config.rb, add favicon and more

--- contact.php
+++ contact.php
@@ -8,6 +8,7 @@
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600|Montserrat:400,700' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="stylesheets/style.css">
+  <link rel="icon" type="image/png" href="images/favicon.png" />
   <title>Steve Lorraine - Portfolio</title>
   <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
